3ac582bbc91662ce837f1a6dfa4d173649278ed2
Choiwonjae <wjchoi1221@naver.com>
2024-08-13 15:40:58 +0900

    백준 11279 해결

---
+++ heap.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+
+int main(void){
+    priority_queue <int> pq;
+    vector <int> vc;
+    int count;
+    cin >> count;
+    for (int i = 0; i < count; i++)
+    {
+        int empty;
+        cin >> empty;
+        if (empty == 0)
+        {
+            if (pq.empty())
+            {
+                vc.push_back(0);
+            }
+            else  {
+                vc.push_back(pq.top());
+                pq.pop();
+            }
+            
+        }
+        else{
+            pq.push(empty);
+        }
+        
+    }
+    for (int i = 0; i < vc.size(); i++)
+    {
+        cout << vc[i] << "\n"; 
+    }
+    
+    
+}
